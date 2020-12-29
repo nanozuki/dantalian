71865690bfa69e3fb35d40f5a087195adf19933e
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-12-29 14:29:42 +0800

    add opts' parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Arg, App, ArgSettings, crate_name, crate_authors, crate_description, crate_version};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .arg(Arg::new("roots")
+            .long("roots")
+            .about("set media file's roots")
+            .settings(&[ArgSettings::MultipleOccurrences, ArgSettings::TakesValue])
+            .required(true))
+        .get_matches();
+    let roots: Vec<&str> = matches.values_of("roots").unwrap().collect();
+    for root in roots {
+        println!("root: {}", root)
+    }
 }
