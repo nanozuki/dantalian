d5b96df4507e7501c585c1d1a2a5a21fadca878d
gyara <me@gyara.moe>
2020-12-29 19:30:42 +0900

    bangumi type SubjectSmall

---
+++ src/bangumi/types.rs
@@ -0,0 +1,77 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub enum SubjectType {
+    Book = 1,
+    Anime = 2,
+    Music = 3,
+    Game = 4,
+    Real = 6,
+}
+
+#[derive(Deserialize)]
+pub struct SubjectImage {
+    pub description: String,
+    pub large: String,
+    pub common: String,
+    pub medium: String,
+    pub small: String,
+    pub grid: String,
+}
+
+#[derive(Deserialize)]
+pub struct SubjectRatingCount {
+    #[serde(rename = "1")]
+    pub s1: u32,
+    #[serde(rename = "2")]
+    pub s2: u32,
+    #[serde(rename = "3")]
+    pub s3: u32,
+    #[serde(rename = "4")]
+    pub s4: u32,
+    #[serde(rename = "5")]
+    pub s5: u32,
+    #[serde(rename = "6")]
+    pub s6: u32,
+    #[serde(rename = "7")]
+    pub s7: u32,
+    #[serde(rename = "8")]
+    pub s8: u32,
+    #[serde(rename = "9")]
+    pub s9: u32,
+    #[serde(rename = "10")]
+    pub s10: u32,
+}
+
+#[derive(Deserialize)]
+pub struct SubjectRating {
+    pub total: u32,
+    pub score: f32,
+    pub count: SubjectRatingCount,
+}
+
+#[derive(Deserialize)]
+pub struct SubjectCollection {
+    pub wish: u32,
+    pub collect: u32,
+    pub doing: u32,
+    pub on_hold: u32,
+    pub dropped: u32,
+}
+
+#[derive(Deserialize)]
+pub struct SubjectSmall {
+    pub id: u32,
+    pub url: String,
+    #[serde(rename = "type")]
+    pub subject_type: SubjectType,
+    pub name: String,
+    pub name_cn: String,
+    pub summary: String,
+    pub air_date: String,
+    pub air_weekday: u8,
+    pub images: SubjectImage,
+    pub rank: u32,
+    pub rating: SubjectRating,
+    pub collection: SubjectCollection,
+}
