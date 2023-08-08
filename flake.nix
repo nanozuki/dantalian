f6eeff59bf5061cfd33052806b31117ca19c6c9a
ギャラ <me@gyara.moe>
2023-08-08 14:01:12 +0900

    feat(flake): Use devshell

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ flake-parts, naersk, ... }:
+  outputs = inputs@{ flake-parts, naersk, devshell, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        devshell.flakeModule
+      ];
+
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let naersk-lib = pkgs.callPackage naersk { }; in
@@ -21,6 +26,9 @@
             buildInputs = with pkgs; [ openssl ];
           };
           packages.default = self'.packages.dantalian;
+          devshells.default = {
+            packages = with pkgs; [ cargo clippy rustfmt ];
+          };
 
           formatter = pkgs.nixpkgs-fmt;
         };
